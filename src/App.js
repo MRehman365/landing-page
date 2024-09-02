4843bd8042c296fd3f3544d90d2900b580c8e141
suryabhai9012 <surajsingh901276@gmail.com>
2024-09-02 18:02:36 +0530

    add routing
    add routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import './App.css';
-import Body from './Components/Body';
-import Footer from './Components/Footer';
-import Navbar from './Components/Navbar';
+import Body from "./Components/Body";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
 
 function App() {
   return (
-  <>
-  <Navbar />
-  <Body />
-  <Footer />
-  </>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/servise" element={<Services />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
